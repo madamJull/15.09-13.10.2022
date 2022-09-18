249ac1afca5431cfbc6db6db0aadb3e63b760dfa
Juliya <9123288799@mail/ru>
2022-09-18 23:58:55 +0500

    Add Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два целых числа для того, чтобы узнать является ли первое число квадратом второго");
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int square = numberB * numberB;
+if (square == numberA)
+{
+    Console.WriteLine($"Первое число {numberA} является кваратом второго числа {numberB}");
+}
+else
+{
+    Console.WriteLine($"Первое число {numberA} не является кваратом второго числа {numberB}");
+}
