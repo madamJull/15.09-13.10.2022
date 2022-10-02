cb4f14882bf241505f0fd8c0ab13cb93ce71a257
Juliya <9123288799@mail/ru>
2022-10-02 22:27:52 +0500

    Add Task05

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ведите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
---
+++ Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = -num;
+while(x <= num)
+{
+    Console.Write($"{x},");
+    x++;
+}
