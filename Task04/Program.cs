b72235abd8305a887ad37dc3e7a743d251d1d838
Juliya <9123288799@mail/ru>
2022-10-02 23:12:22 +0500

    Add Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите три числа, чтобы узнать максимальное из них");
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberD = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberD > max) max = numberD;
+Console.WriteLine($"Число {max} является максимальным");
