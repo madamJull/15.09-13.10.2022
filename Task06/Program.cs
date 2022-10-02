cad831c94edb3c73be175ba2200b583bf4a7cfb5
Juliya <9123288799@mail/ru>
2022-10-02 23:35:32 +0500

    Add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ведите целое числа, чтобы узнать является ли оно четным: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+Console.WriteLine("Да");
+else 
+{
+    Console.WriteLine("Нет");
+}
