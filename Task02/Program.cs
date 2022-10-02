97213dc5365b14ed88429cb7696c01d1b7ee9e15
Juliya <9123288799@mail/ru>
2022-10-02 22:37:52 +0500

    Add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ведите два числа, чтобы узнать какое из них большее, а какое меньшее");
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"Первое число {numberA} является максимальным, второе число {numberB} является минимальным");
+}
+else
+{
+    Console.WriteLine($"Второе число {numberB} является максимальным, первое число {numberA} является минимальным");
+}
