44fb158224d1f2940efe2cc2419482a961a4659f
Juliya <9123288799@mail/ru>
2022-10-02 15:05:39 +0500

    Add Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзачное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 10;
+Console.WriteLine($"Последней цифрой числа {number} является {result}");
