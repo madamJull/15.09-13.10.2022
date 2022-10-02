3af4984a23815a4c66fffa3805cd150e53226495
Juliya <9123288799@mail/ru>
2022-10-02 14:24:58 +0500

    Add 03

---
+++ Task03/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+string numDay = Console.ReadLine();
+if (numDay == "1")
+{
+    Console.WriteLine("Monday");
+}
+else if(numDay == "2")
+{
+    Console.WriteLine("Tuesday");
+}
+else if(numDay == "3")
+{
+    Console.WriteLine("Wednesday");
+}
+else if(numDay == "4")
+{
+    Console.WriteLine("Thursday");
+}
+else if(numDay == "5")
+{
+    Console.WriteLine("Friday");
+}
+else if(numDay == "6")
+{
+    Console.WriteLine("Saturday");
+}
+else if(numDay == "7")
+{
+    Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("Вы ввели не верное число");
+}
