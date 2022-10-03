97166ae4635f4268dc62c64c500f1cdcdc0a85bf
Juliya <9123288799@mail/ru>
2022-10-03 22:08:11 +0500

    Add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число, чтобы узнать все четные значения от 1 до введенного числа");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+while(x <= num)
+{
+    if (x % 2 == 0)
+    Console.Write($"{x },");
+    {
+        x++;
+    }
+}
+
+
