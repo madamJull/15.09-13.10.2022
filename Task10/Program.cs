88b5018d5323be42225ee9f4f573161ffc0f89e7
Juliya <9123288799@mail/ru>
2022-11-28 18:41:21 +0500

    Add 10

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзачное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 100/10;
+Console.WriteLine($"Второй цифрой числа {number} является {result}");
