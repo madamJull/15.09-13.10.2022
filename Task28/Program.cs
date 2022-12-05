40b2e9ff836bff2a06a4d910776a49719211cf8f
Juliya <9123288799@mail/ru>
2022-12-05 22:16:39 +0500

    Add 28

---
+++ Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число M и N и выдаёт произведение чисел от M до N.
+// 2,4 -> 24 
+// 1,5 -> 120
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsCorrectNumbers(m, n) ? $"Произведение чисел {m} и {n} = {FindProductNumbers(m, n)}" : "Введенные числа заданы неверно");
+
+int FindProductNumbers(int num1, int num2)
+{
+    int prod = 1;
+    for (int i = num1; i <= num2; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+}
+bool IsCorrectNumbers(int num1, int num2)
+{
+    if(num1 < num2 && num1 != 0) return true;
+    else return false;
+}
