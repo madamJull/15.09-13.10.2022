8120de03835f849e0e1a10d18b842a5e0298cc88
Juliya <9123288799@mail/ru>
2022-12-07 19:04:15 +0500

    Add 31

---
+++ Task31/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+//Найдите сумму отрицательных и положительных элементов массива.
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+Console.Clear();
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное допустимое значение в массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное допустимое значение в массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(size, min, max);
+
+Console.WriteLine($"Получившийся массив: [{string.Join(",", array)}]");
+
+Console.WriteLine($"Сумма положительных элементов в массиве: {FindSumPossitiveAndNegativeByArray(array).Item1}");
+Console.WriteLine($"Сумма отрицательных элементов в массиве: {FindSumPossitiveAndNegativeByArray(array).Item2}");
+
+(int, int) FindSumPossitiveAndNegativeByArray(int[] inputArray)
+{
+    int sumPossitiveValues = 0;
+    int sumNegativeValues = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i] > 0)
+            sumPossitiveValues += inputArray[i]; // sumPossitiveValues = sumPossitiveValues + inputArray[i];
+        else
+            sumNegativeValues += inputArray[i];
+    }
+    return (sumPossitiveValues, sumNegativeValues);
+}
+
+int[] FillArray(int sizeArray, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[] resultArray = new int[sizeArray];
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = random.Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
