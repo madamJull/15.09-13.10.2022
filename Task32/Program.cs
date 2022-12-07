05b2fd479347712a94d2bace993a75e07f9b81ac
Juliya <9123288799@mail/ru>
2022-12-07 19:59:40 +0500

    Add 32

---
+++ Task32/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+Console.Clear();
+Console.Write("Введите число элементов массива : ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(size, -99, 99);
+Console.WriteLine($"Оригинальный массив:    [ {string.Join(", ", array)} ]");
+
+array = InverseArray(array);
+Console.WriteLine($"Инвертированный массив: [ {string.Join(", ", array)} ]");
+
+int[] InverseArray(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++) arr[i] *= -1;
+    return arr;
+}
+
+int[] FillArray(int sizeArray, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] resultArray = new int[sizeArray];
+    for (int i = 1; i < resultArray.Length; i++)
+    {
+        resultArray[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
