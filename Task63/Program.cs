5f3a8f0070c8408f119ead84166e181ae731065e
Juliya <9123288799@mail/ru>
2022-12-22 20:38:39 +0500

    Add 63

---
+++ Task63/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N с помощью рекурсии
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+
+Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintValue (1, n); 
+
+void PrintValue (int m, int max)
+{
+if (m > max) return;
+Console.Write($" {m} ");
+PrintValue(m + 1, n);
+}
